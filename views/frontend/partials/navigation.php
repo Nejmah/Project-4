8bba79addc54d8ba9318c4fd3e7d6654009d1adb
aurele <aurelie.nejmah@gmail.com>
2020-08-01 16:27:28 +0200

    Responsive navigation

--- views/frontend/partials/navigation.php
+++ views/frontend/partials/navigation.php
@@ -1,12 +1,14 @@
 <nav class="navbar navbar-expand-lg navbar-light" id="navigation">
-    <a class="navbar-brand" href="<?= env("URL_PREFIX") ?>"><img class="moutain-img" src="<?= env("URL_PREFIX") ?>/assets/img/mountain.png" alt="mountain"></a>
+    <a class="navbar-brand" href="<?= env("URL_PREFIX") . "/" ?>">
+        <img class="moutain-img" src="<?= env("URL_PREFIX") ?>/assets/img/mountain.png" alt="mountain">
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item <?php if ($metaTitle == 'Accueil') {echo 'active';} ?>">
-                <a class="nav-link" href="<?= env("URL_PREFIX") ?>">ACCUEIL</a>
+                <a class="nav-link" href="<?= env("URL_PREFIX") . "/" ?>">ACCUEIL</a>
             </li>
             <li class="nav-item <?php if ($metaTitle == 'A propos') {echo 'active';} ?>">
                 <a class="nav-link" href="<?= env("URL_PREFIX") ?>/about">A PROPOS</a>
