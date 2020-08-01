f2de384137d73218495fff5f2da5ec6ed05ca42f
aurele <aurelie.nejmah@gmail.com>
2020-08-01 16:08:03 +0200

    Responsive page d'accueil

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(function () {
+    // Code pour centrer les éléments verticalement sur la page home et login
+    if ($(".home-page").length > 0) {
+        // Si on est sur la page d'accueil
+        $("header").addClass("fixed-header");
+    }
+});
--- views/frontend/template.php
+++ views/frontend/template.php
@@ -10,6 +10,7 @@
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 		<link rel="stylesheet" href="<?= env("URL_PREFIX") ?>/assets/css/style.css">
+		<link rel="stylesheet" href="<?= env("URL_PREFIX") ?>/assets/css/responsive.css">
         <link rel="stylesheet" href="<?= env("URL_PREFIX") ?>/assets/css/fonts.css">
   	</head>
 	
@@ -35,5 +36,6 @@
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 		<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 		<script src="<?= env("URL_PREFIX") ?>/assets/js/modal.js"></script>
+		<script src="<?= env("URL_PREFIX") ?>/assets/js/script.js"></script>
 	</body>
 </html>
